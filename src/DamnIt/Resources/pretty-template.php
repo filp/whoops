defbe18a42a0588d8ee76cdec50eebc1cfad37e1
filp <dobreira@gmail.com>
2013-03-11 23:21:43 +0000

    Add skeleton for PrettyPage handler template

---
+++ src/DamnIt/Resources/pretty-template.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Template file for DamnIt's pretty error output.
+* Check the $v global variable (stdClass) for what's available
+* to work with.
+* @var $v
+*/
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Damn it! - An error ocurred.</title>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/2.1.0/normalize.css">
+    <style>
+
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>There was an error</h1>
+      <p>
+        But it's gone now.
+      </p>
+    </div>
+  </body>
+</html>
