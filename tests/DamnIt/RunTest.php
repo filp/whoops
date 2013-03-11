4a03ee17b5aa59e9b7e7607a0305b9ddfab1314f
filp <dobreira@gmail.com>
2013-03-11 21:50:02 +0000

    Add Handler sorting tests

--- tests/DamnIt/RunTest.php
+++ tests/DamnIt/RunTest.php
@@ -7,6 +7,7 @@
 namespace DamnIt;
 use DamnIt\TestCase;
 use DamnIt\Run;
+use Damnit\Handler\Handler;
 use DamnIt\Handler\DummyHandler;
 use \RuntimeException;
 
@@ -86,5 +87,48 @@ class RunTest extends TestCase
 
         $run->unregister();
         throw new RuntimeException("I'm not supposed to be caught!");
+    }
+
+    /**
+     * @covers DamnIt\Run::addHandler
+     * @covers DamnIt\Run::getHandlers
+     */
+    public function testHandlerSorting()
+    {
+        $run = $this->getRunInstance();
+
+        $handlerOne   = new DummyHandler;
+        $handlerTwo   = new DummyHandler;
+        $handlerThree = new DummyHandler;
+        $handlerFour  = new DummyHandler;
+
+        // Priority with integer, or no priority:
+        $run->addHandler($handlerOne, 10);
+        $run->addHandler($handlerTwo, 0);
+        $run->addHandler($handlerThree, 25);
+        $run->addHandler($handlerFour, -10);
+
+        $handlers = $run->getHandlers();
+        
+        $this->assertSame($handlers[0], $handlerFour);
+        $this->assertSame($handlers[1], $handlerTwo);
+        $this->assertSame($handlers[2], $handlerOne);
+        $this->assertSame($handlers[3], $handlerThree);
+
+        // Use priority constants to ensure handlers
+        // go either to the end or start of the stack
+        $run->clearHandlers();
+
+        $run->addHandler($handlerOne, Handler::HIGH_PRIORITY);
+        $run->addHandler($handlerTwo, Handler::HIGH_PRIORITY);
+        $run->addHandler($handlerThree, -2);
+        $run->addHandler($handlerFour, Handler::LOW_PRIORITY);
+
+        $handlers = $run->getHandlers();
+
+        $this->assertSame($handler[0], $handlerFour);
+        $this->assertSame($handler[1], $handlerThree);
+        $this->assertSame($handler[2], $handlerOne);
+        $this->assertSame($handler[3], $handlerTwo);
     }
 }
