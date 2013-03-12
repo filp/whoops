b796a026a266fad9b9756a7becf0b6fdd5d277d4
filp <dobreira@gmail.com>
2013-03-12 12:55:54 +0000

    Update Damnit\Exception\Frame, add tests

--- src/Damnit/Exception/Frame.php
+++ src/Damnit/Exception/Frame.php
@@ -5,13 +5,19 @@
  */
 
 namespace Damnit\Exception;
+use \InvalidArgumentException;
 
 class Frame
 {
     /**
      * @var array
      */
-    private $frame;
+    protected $frame;
+
+    /**
+     * @var string
+     */
+    protected $fileContentsCache;
 
     /**
      * @param array[]
@@ -19,5 +25,96 @@ class Frame
     public function __construct(array $frame)
     {
         $this->frame = $frame;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFile()
+    {
+        return !empty($this->frame['file']) ? $this->frame['file'] : null;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getLine()
+    {
+        return isset($this->frame['line']) ? $this->frame['line'] : null;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getClass()
+    {
+        return isset($this->frame['class']) ? $this->frame['class'] : null;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFunction()
+    {
+        return isset($this->frame['function']) ? $this->frame['function'] : null;
+    }
+
+    /**
+     * @return array
+     */
+    public function getArgs()
+    {
+        return isset($this->frame['args']) ? (array) $this->frame['args'] : array();
+    }
+
+    /**
+     * Returns the full contents of the file for this frame,
+     * if it's known.
+     * @return string|null
+     */
+    public function getFileContents()
+    {
+        if($this->fileContentsCache === null && $filePath = $this->getFile()) {
+            $this->fileContentsCache = file_get_contents($filePath);
+        }
+
+        return $this->fileContentsCache;
+    }
+
+    /**
+     * Returns the contents of the file for this frame as an
+     * array of lines, and optionally as clamped range of lines.
+     *
+     * @example
+     *     $frame->getFileLines(); // => array( 0 => '<?php', 1 => '...', ...)
+     * @example
+     *     $frame->getFileLines(10, 15); // array( 10 => '...', 11 => '...', ...)
+     *
+     * @param  int $start
+     * @param  int $end
+     * @return array|null
+     */
+    public function getFileLines($start = 0, $end = null)
+    {
+        if(null !== ($contents = $this->getFileContents())) {
+            $lines = explode("\n", $contents);
+
+            // Get a subset of lines from $start to $end
+            if($end !== null)
+            {
+                $start = (int) $start;
+                $end   = (int) $end;
+
+                if($end <= $start) {
+                    throw new InvalidArgumentException(
+                        "\$end($end) cannot be lower or equal to \$start($start)"
+                    );
+                }
+
+                $lines = array_slice($lines, $start, $end, true);
+            }
+
+            return $lines;
+        }
     }
 }
---
+++ tests/Damnit/Exception/FrameTest.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Damnit - php errors for cool kids
+ * @author Filipe Dobreira <http://github.com/filp>
+ */
+
+namespace Damnit\Exception;
+use Damnit\Exception\Frame;
+use Damnit\TestCase;
+use \Mockery as m;
+
+class FrameTest extends TestCase
+{
+    /**
+     * @return array
+     */
+    private function getFrameData()
+    {
+        return array(
+            'file'     => __DIR__ . '/../../Fixtures/frame.lines-test.php',
+            'line'     => 0,
+            'function' => 'test',
+            'class'    => 'MyClass',
+            'args'     => array(true, 'hello')
+        );
+    }
+
+    /**
+     * @return Damnit\Exception\Frame
+     */
+    private function getFrameInstance($data = null)
+    {
+        if($data === null) {
+            $data = $this->getFrameData();
+        }
+
+        return new Frame($data);
+    }
+
+    /**
+     * @covers Damnit\Exception\Frame::getFile
+     */
+    public function testGetFile()
+    {
+        $data  = $this->getFrameData();
+        $frame = $this->getFrameInstance($data);
+
+        $this->assertEquals($frame->getFile(), $data['file']);
+    }
+
+    /**
+     * @covers Damnit\Exception\Frame::getLine
+     */
+    public function testGetLine()
+    {
+        $data  = $this->getFrameData();
+        $frame = $this->getFrameInstance($data);
+
+        $this->assertEquals($frame->getLine(), $data['line']);
+    }
+
+    /**
+     * @covers Damnit\Exception\Frame::getClass
+     */
+    public function testGetClass()
+    {
+        $data  = $this->getFrameData();
+        $frame = $this->getFrameInstance($data);
+
+        $this->assertEquals($frame->getClass(), $data['class']);
+    }
+
+    /**
+     * @covers Damnit\Exception\Frame::getFunction
+     */
+    public function testGetFunction()
+    {
+        $data  = $this->getFrameData();
+        $frame = $this->getFrameInstance($data);
+
+        $this->assertEquals($frame->getFunction(), $data['function']);
+    }
+
+    /**
+     * @covers Damnit\Exception\Frame::getArgs
+     */
+    public function testGetArgs()
+    {
+        $data  = $this->getFrameData();
+        $frame = $this->getFrameInstance($data);
+
+        $this->assertEquals($frame->getArgs(), $data['args']);
+    }
+
+    /**
+     * @covers Damnit\Exception\Frame::getFileContents
+     */
+    public function testGetFileContents()
+    {
+        $data  = $this->getFrameData();
+        $frame = $this->getFrameInstance($data);
+
+        $this->assertEquals($frame->getFileContents(), file_get_contents($data['file']));
+    }
+
+    /**
+     * @covers Damnit\Exception\Frame::getFileLines
+     */
+    public function testGetFileLines()
+    {
+        $data  = $this->getFrameData();
+        $frame = $this->getFrameInstance($data);
+
+        $lines = explode("\n", $frame->getFileContents());
+        $this->assertEquals($frame->getFileLines(), $lines);
+    }
+
+    /**
+     * @covers Damnit\Exception\Frame::getFileLines
+     */
+    public function testGetFileLinesRange()
+    {
+        $data  = $this->getFrameData();
+        $frame = $this->getFrameInstance($data);
+
+        $lines = $frame->getFileLines(1, 3);
+
+        $this->assertEquals($lines[1], '// Line 2');
+        $this->assertEquals($lines[2], '// Line 3');
+        $this->assertEquals($lines[3], '// Line 4');
+    }
+}
