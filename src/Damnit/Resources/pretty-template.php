5163d1f6593e298af5eaf0eb3e12ddb09187ea6f
filp <dobreira@gmail.com>
2013-03-15 18:52:39 +0000

    Increase header height/padding

--- src/Damnit/Resources/pretty-template.php
+++ src/Damnit/Resources/pretty-template.php
@@ -48,12 +48,12 @@
     }
     header {
       width: 100%;
-      height: 80px;
+      height: 110px;
       position: absolute;
       z-index: 99;
       top: 0;
       left: 0;
-      padding: 10px 20px;
+      padding: 30px 20px;
       color: white;
       background: #272727;
     }
@@ -69,7 +69,7 @@
         }
     .stack-container{
         height: 100%;
-        padding-top:80px;
+        padding-top:110px;
         position: relative;
     }
     .details-container {
