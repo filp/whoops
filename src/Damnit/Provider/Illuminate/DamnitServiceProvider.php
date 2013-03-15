aa3697857c1cc9f161245efa3b67164ef4ec2dcb
filp <dobreira@gmail.com>
2013-03-15 20:58:46 +0000

    CS Fix Illuminate Provider (tabs -> spaces)

--- src/Damnit/Provider/Illuminate/DamnitServiceProvider.php
+++ src/Damnit/Provider/Illuminate/DamnitServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Damnit - php errors for cool kids
- * @author @schickling <https://github.com/schickling>
+ * @author Filipe Dobreira <https://github.com/filp>
  */
 
 namespace Damnit\Provider\Illuminate;
@@ -11,28 +11,29 @@ use Illuminate\Support\ServiceProvider;
 
 /**
  * Provides support for Illuminate (Laravel 4)
+ * @author schickling <https://github.com/schickling>
  */
 class DamnitServiceProvider extends ServiceProvider {
 
-	/**
-	 * Register the service provider for damnit to laravel
-	 */
-	public function register()
-	{
-		$app = $this->app;
+    /**
+     * Register the service provider for damnit to laravel
+     */
+    public function register()
+    {
+        $app = $this->app;
 
-		$app['damnit.handler'] = $app->share(function($app) {
-			return new PrettyPageHandler;
-		});
+        $app['damnit.handler'] = $app->share(function($app) {
+            return new PrettyPageHandler;
+        });
 
-		$app['damnit'] = $app->share(function($app) {
-			$run = new Run;
+        $app['damnit'] = $app->share(function($app) {
+            $run = new Run;
 
-			return $run->pushHandler($app['damnit.handler']);
-		});
+            return $run->pushHandler($app['damnit.handler']);
+        });
 
-		$app->error(function($e) use ($app) {
-			$app['damnit']->handleException($e);
-		});
-	}
+        $app->error(function($e) use ($app) {
+            $app['damnit']->handleException($e);
+        });
+    }
 }
