1ef3791fa83f1c2b7a48a0b06b7fe20f1f89f4fb
Joao Gilberto Magalhaes <joao@byjg.com.br>
2013-10-16 16:09:47 -0300

    Coded XmlResponseHandler::toXml() from scratch

--- src/Whoops/Handler/XmlResponseHandler.php
+++ src/Whoops/Handler/XmlResponseHandler.php
@@ -36,61 +36,35 @@ class XmlResponseHandler extends Handler
     }
 
 	/**
-	 * The main function for converting to an XML document.
-	 * Pass in a multi dimensional array and this recrusively loops through and builds up an XML document.
 	 *
-	 * @author  k dot antczak at livedata dot pl
-	 * @date    2011-04-22 06:08 UTC
-	 * @link    http://snipplr.com/view/3491/
-	 * @param array $data
-	 * @param string $rootNodeName - what you want the root node to be - defaultsto data.
-	 * @param SimpleXMLElement $xml - should only be used recursively
-	 * @return string XML
+	 * @param array $array
+	 * @param string $rootNodeName
+	 * @return string
 	 */
-	public function toXml($data, $rootNodeName = 'root', $xml=null)
+	public function toXml($array, $rootNodeName = 'root')
 	{
-		// turn off compatibility mode as simple xml throws a wobbly if you don't.
-		if (ini_get('zend.ze1_compatibility_mode') == 1)
-		{
-			ini_set ('zend.ze1_compatibility_mode', 0);
-		}
-
-		if ($xml == null)
-		{
-			$xml = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><$rootNodeName />");
-		}
-
-		// loop through the data passed in.
-		foreach($data as $key => $value)
-		{
-			// no numeric keys in our xml please!
-			if (is_numeric($key))
-			{
-				// make string key...
-				$key = "unknownNode_". (string) $key;
-			}
-
-			// delete any char not allowed in XML element names
-            $key = preg_replace('/[^a-z0-9\-\_\.\:]/i', '', $key);
-
-			// if there is another array found recrusively call this function
-			if (is_array($value))
-			{
-				$node = $xml->addChild($key);
-				// recrusive call.
-				$this->toXml($value, $rootNodeName, $node);
-			}
-			else
-			{
-				// add single node.
-				$value = str_replace('&', '&amp;', print_r($value, true));
-				$xml->addChild($key,$value);
-			}
-		}
-		// pass back as string. or simple xml object if you want!
-		return $xml->asXML();
+		return $this->_toXml($array, new \SimpleXMLElement('<' . $rootElement . ' />'));
 	}
 
+	/**
+	 *
+	 * @param array $array
+	 * @param \SimpleXMLElement $xml
+	 * @return string
+	 */
+	private function _toXml($array, $xml)
+	{
+		foreach ($array as $key => $value) {
+			if (is_array($value)) {
+				$child = $xml->addChild($key);
+				$this->_toXml($value, $child);
+			}
+			else {
+				$xml->addChild($key, $value);
+			}
+		}
+		return $xml->asXML();
+	}
 	/**
      * @return int
      */
