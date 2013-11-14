d45823aae6eb44edc46f750f9f8c9c2412ba2fa0
Denis Sokolov <denis@sokolov.cc>
2013-11-14 20:04:47 +0100

    XmlResponseHandler::toXml: refactor

--- src/Whoops/Handler/XmlResponseHandler.php
+++ src/Whoops/Handler/XmlResponseHandler.php
@@ -37,58 +37,24 @@ class XmlResponseHandler extends Handler
 
     /**
      * The main function for converting to an XML document.
-     * Pass in a multi dimensional array and this recrusively loops through and builds up an XML document.
      *
-     * @author  k dot antczak at livedata dot pl
-     * @date    2011-04-22 06:08 UTC
-     * @link    http://snipplr.com/view/3491/
-     * @param array $data
-     * @param string $rootNodeName - what you want the root node to be - defaultsto data.
-     * @param SimpleXMLElement $xml - should only be used recursively
+     * @param array|Traversable $data
+     * @param string $rootNodeName - what you want the root node to be
      * @return string XML
      */
-    public function toXml($data, $rootNodeName = 'root', $xml=null)
+    public function toXml($data, $rootNodeName = 'root')
     {
+        assert('is_array($data) || $node instanceof Traversable');
+
         // turn off compatibility mode as simple xml throws a wobbly if you don't.
         if (ini_get('zend.ze1_compatibility_mode') == 1)
         {
             ini_set ('zend.ze1_compatibility_mode', 0);
         }
 
-        if ($xml == null)
-        {
-            $xml = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><$rootNodeName />");
-        }
+        $node = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><$rootNodeName />");
 
-        // loop through the data passed in.
-        foreach($data as $key => $value)
-        {
-            // no numeric keys in our xml please!
-            if (is_numeric($key))
-            {
-                // make string key...
-                $key = "unknownNode_". (string) $key;
-            }
-
-            // delete any char not allowed in XML element names
-            $key = preg_replace('/[^a-z0-9\-\_\.\:]/i', '', $key);
-
-            // if there is another array found recrusively call this function
-            if (is_array($value))
-            {
-                $node = $xml->addChild($key);
-                // recrusive call.
-                $this->toXml($value, $rootNodeName, $node);
-            }
-            else
-            {
-                // add single node.
-                $value = str_replace('&', '&amp;', print_r($value, true));
-                $xml->addChild($key,$value);
-            }
-        }
-        // pass back as string. or simple xml object if you want!
-        return $xml->asXML();
+        return self::addDataToNode($node, $data)->asXML();
     }
 
     /**
@@ -129,5 +95,40 @@ class XmlResponseHandler extends Handler
         echo $this->toXml($response);
 
         return Handler::QUIT;
+    }
+
+    /**
+     * @param SimpleXMLElement $node Node to append data to, will be modified in place
+     * @param array|Traversable $data
+     * @return SimpleXMLElement The modified node, for chaining
+     */
+    private static function addDataToNode(\SimpleXMLElement $node, $data)
+    {
+        assert('is_array($data) || $node instanceof Traversable');
+
+        foreach($data as $key => $value)
+        {
+            if (is_numeric($key))
+            {
+                // Convert the key to a valid string
+                $key = "unknownNode_". (string) $key;
+            }
+
+            // Delete any char not allowed in XML element names
+            $key = preg_replace('/[^a-z0-9\-\_\.\:]/i', '', $key);
+
+            if (is_array($value))
+            {
+                $child = $node->addChild($key);
+                self::addDataToNode($child, $value);
+            }
+            else
+            {
+                $value = str_replace('&', '&amp;', print_r($value, true));
+                $node->addChild($key, $value);
+            }
+        }
+
+        return $node;
     }
 }
