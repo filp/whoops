filp/whoops

commit 70e23d1c5225af13f964e78edf38e0c5bef4c20f

TemplateHelper: improve invalid utf8 handling in hhvm

src/Whoops/Util/TemplateHelper.php
@@ -27,7 +27,9 @@ class TemplateHelper
     {
         $flags = ENT_QUOTES;
 
-        if (defined("ENT_SUBSTITUTE")) {
+	// HHVM has all constants defined, but only ENT_IGNORE
+	// works at the moment
+        if (defined("ENT_SUBSTITUTE") && !defined("HHVM_VERSION")) {
             $flags |= ENT_SUBSTITUTE;
         } else {
             // This is for 5.3.

tests/Whoops/Util/TemplateHelperTest.php
@@ -39,10 +39,6 @@ class TemplateHelperTest extends TestCase
 
     public function testEscapeBrokenUtf8()
     {
-        if (defined('HHVM_VERSION')) {
-            return $this->markTestSkipped();
-        }
-
         // The following includes an illegal utf-8 sequence to test.
         // Encoded in base64 to survive possible encoding changes of this file.
         $original = base64_decode('VGhpcyBpcyBhbiBpbGxlZ2FsIHV0Zi04IHNlcXVlbmNlOiDD');

commit f6b790e41c4a870b4ebcd09b2a13840ed2263d1a

TemplateHelper: fix whitespace broken by GitHub editor in 70e23d1c

src/Whoops/Util/TemplateHelper.php
@@ -27,8 +27,8 @@ class TemplateHelper
     {
         $flags = ENT_QUOTES;
 
-	// HHVM has all constants defined, but only ENT_IGNORE
-	// works at the moment
+        // HHVM has all constants defined, but only ENT_IGNORE
+        // works at the moment
         if (defined("ENT_SUBSTITUTE") && !defined("HHVM_VERSION")) {
             $flags |= ENT_SUBSTITUTE;
         } else {