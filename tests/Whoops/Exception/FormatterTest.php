04a3f9b8c1ba398159fd0293fb8694cde126bde6
Denis Sokolov <denis@sokolov.cc>
2014-05-22 00:25:59 +0900

    Formatter: a simple test for plain

---
+++ tests/Whoops/Exception/FormatterTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Whoops - php errors for cool kids
+ * @author Filipe Dobreira <http://github.com/filp>
+ */
+
+namespace Whoops\Exception;
+use Whoops\TestCase;
+
+class FormatterTest extends TestCase
+{
+	public function testPlain()
+	{
+		$msg = 'Sample exception message foo';
+		$output = Formatter::formatExceptionPlain(new Inspector(new \Foo\Bar\Quux\Exception($msg)));
+		$this->assertContains($msg, $output);
+		$this->assertContains('Stacktrace', $output);
+	}
+}
