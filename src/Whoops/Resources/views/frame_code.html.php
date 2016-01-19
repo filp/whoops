0e5b4feec16de848c88f73968e6f13a576601ec7
Youssef <youssefhad2@gmail.com>
2016-01-19 21:14:58 +0000

    highlight line error

--- src/Whoops/Resources/views/frame_code.html.php
+++ src/Whoops/Resources/views/frame_code.html.php
@@ -29,10 +29,20 @@
             $start = key($range) + 1;
             $code  = join("\n", $range);
         ?>
-            <style>#frame-code-0 .linenums li:nth-child(<?= $line-$start+1 ?>) {
-                /* Style current line */
-                background-color: hsl(0, 0%, 33%); 
-            }</style>
+            <style>
+             #frame-code-0 .linenums li:nth-child(<?= $line-$start ?>) {
+                 background-color: rgba(255, 100, 100, .07); 
+                 padding: 2px;
+              }
+             #frame-code-0 .linenums li:nth-child(<?= $line-$start+1 ?>) {
+                 background-color: rgba(255, 100, 100, .17); 
+                 padding: 2px;
+              }
+             #frame-code-0 .linenums li:nth-child(<?= $line-$start+2 ?>) {
+                 background-color: rgba(255, 100, 100, .07); 
+                 padding: 2px;
+              }
+            </style>
             <pre class="code-block prettyprint linenums:<?php echo $start ?>"><?php echo $tpl->escape($code) ?></pre>
           <?php endif ?>
         <?php endif ?>
