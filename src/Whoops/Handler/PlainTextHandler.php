fd653d956822f862c0ce16401282cb8677dbf08c
USAMI Kenta <tadsan@zonu.me>
2017-06-25 05:10:20 +0900

    fixup! Impl PlainTextHandler::dump()

--- src/Whoops/Handler/PlainTextHandler.php
+++ src/Whoops/Handler/PlainTextHandler.php
@@ -209,7 +209,7 @@ class PlainTextHandler extends Handler
 
         // Dump the arguments:
         ob_start();
-        $this->dump($frame->getArgs());
+        $this->_dump($frame->getArgs());
         if (ob_get_length() > $this->getTraceFunctionArgsOutputLimit()) {
             // The argument var_dump is to big.
             // Discarded to limit memory usage.
@@ -232,7 +232,7 @@ class PlainTextHandler extends Handler
      * @param mixed $var
      * @return void
      */
-    public function dump($var)
+    public function _dump($var)
     {
         if ($this->dumper) {
             call_user_func($this->dumper, $var);
