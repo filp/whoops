e3ddc1b1b2dcb4fa8d2b66b87361d9b9fb43df42
Daniel Siepmann <coding@daniel-siepmann.de>
2017-11-17 11:39:50 +0100

    BUGFIX: Make application frame selectable
    It was not possible to activate application frame.
    Now the condition is fixed and also css class is fixed.
    
    Therefore one can select to see only application frames.

--- src/Whoops/Resources/views/frames_description.html.php
+++ src/Whoops/Resources/views/frames_description.html.php
@@ -1,13 +1,13 @@
 <div class="frames-description <?php echo $has_frames_tabs ? 'frames-description-application' : '' ?>">
   <?php if ($has_frames_tabs): ?>
     <?php if ($active_frames_tab == 'application'): ?>
-      <a href="#" id="application-frames-tab" class="frames-tab frames-tab-active">
-        Application frames (<?php echo $frames->countIsApplication() ?>)
-      </a>
-    <?php else: ?>
-      <span href="#" id="application-frames-tab" class="frames-tab">
+      <span href="#" id="application-frames-tab" class="frames-tab frames-tab-active">
         Application frames (<?php echo $frames->countIsApplication() ?>)
       </span>
+    <?php else: ?>
+      <a href="#" id="application-frames-tab" class="frames-tab">
+        Application frames (<?php echo $frames->countIsApplication() ?>)
+      </a>
     <?php endif; ?>
     <a href="#" id="all-frames-tab" class="frames-tab <?php echo $active_frames_tab == 'all' ? 'frames-tab-active' : '' ?>">
       All frames (<?php echo count($frames) ?>)
