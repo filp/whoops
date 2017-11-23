c6081b8838686aa04f1e83ba7e91f78b7b2a23e6
Denis Sokolov <denis@sokolov.cc>
2017-11-23 20:22:11 +0200

    FrameCollection: fix array_filter mutating the array to be assoc, close #550

--- src/Whoops/Exception/FrameCollection.php
+++ src/Whoops/Exception/FrameCollection.php
@@ -42,7 +42,7 @@ class FrameCollection implements ArrayAccess, IteratorAggregate, Serializable, C
      */
     public function filter($callable)
     {
-        $this->frames = array_filter($this->frames, $callable);
+        $this->frames = array_values(array_filter($this->frames, $callable));
         return $this;
     }
 
