1b17a9c675c1c27ae9962833d1a4b9cdcd9ca55a
Michael Käfer <michael.kaefer1@gmx.at>
2018-12-08 19:57:14 +0100

    Replace "E_ALL | E_STRICT"  with "E_ALL"
    See my more detailed explanation here: https://stackoverflow.com/questions/1638238/whats-the-point-of-e-all-e-strict-if-its-the-same-value-as-e-all/53685722#53685722

--- src/Whoops/Util/SystemFacade.php
+++ src/Whoops/Util/SystemFacade.php
@@ -26,9 +26,9 @@ class SystemFacade
      */
     public function setErrorHandler(callable $handler, $types = 'use-php-defaults')
     {
-        // Workaround for PHP 5.5
+        // Since PHP 5.4 the constant E_ALL contains all errors (even E_STRICT)
         if ($types === 'use-php-defaults') {
-            $types = E_ALL | E_STRICT;
+            $types = E_ALL;
         }
         return set_error_handler($handler, $types);
     }
