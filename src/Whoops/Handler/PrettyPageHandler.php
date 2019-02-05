661005530592ada61d5c9c23d1b4cfb0f7858ca1
coden <admitrjuk@findevelop.ru>
2019-02-05 16:37:16 +0700

    fix resolver callable phpDoc in addEditor

--- src/Whoops/Handler/PrettyPageHandler.php
+++ src/Whoops/Handler/PrettyPageHandler.php
@@ -391,7 +391,7 @@ class PrettyPageHandler extends Handler
      *       return "http://stackoverflow.com";
      *   });
      * @param string $identifier
-     * @param string $resolver
+     * @param string|callable $resolver
      */
     public function addEditor($identifier, $resolver)
     {
