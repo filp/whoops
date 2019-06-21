114a8f04c178d091de059d54885467df58794619
Denis Sokolov <denis@sokolov.cc>
2019-06-21 12:00:00 +0300

    Fix tab switcher, close #573

--- src/Whoops/Resources/views/frames_description.html.php
+++ src/Whoops/Resources/views/frames_description.html.php
@@ -1,14 +1,8 @@
 <div class="frames-description <?php echo $has_frames_tabs ? 'frames-description-application' : '' ?>">
   <?php if ($has_frames_tabs): ?>
-    <?php if ($active_frames_tab == 'application'): ?>
-      <span href="#" id="application-frames-tab" class="frames-tab frames-tab-active">
+    <a href="#" id="application-frames-tab" class="frames-tab <?php echo $active_frames_tab == 'application' ? 'frames-tab-active' : '' ?>">
         Application frames (<?php echo $frames->countIsApplication() ?>)
-      </span>
-    <?php else: ?>
-      <a href="#" id="application-frames-tab" class="frames-tab">
-        Application frames (<?php echo $frames->countIsApplication() ?>)
-      </a>
-    <?php endif; ?>
+    </a>
     <a href="#" id="all-frames-tab" class="frames-tab <?php echo $active_frames_tab == 'all' ? 'frames-tab-active' : '' ?>">
       All frames (<?php echo count($frames) ?>)
     </a>
